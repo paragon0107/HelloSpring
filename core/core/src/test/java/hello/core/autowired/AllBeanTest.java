e4ffd7e488ac599317f7757a8b6dca37bfc2320c
SHIN MINGYU <smg0725@naver.com>
2024-01-08 19:36:10 +0900

    7-8n

---
+++ core/core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,19 @@
+package hello.core.autowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean(){
+
+    }
+
+    static class DiscountPolicy{
+
+    }
+}
